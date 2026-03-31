a7ad0e37e3bc036349b5126ecddd7b1af424649f
fusiontech21 <hesbackfrfr@gmail.com>
2026-04-01 00:59:12 +0200

    Fixed update.sh

--- Update/update.sh
+++ Update/update.sh
@@ -2,6 +2,6 @@ echo "Updating Fusi..."
 
 curl -L https://github.com/fusiontech21/Fusi/releases/latest/download/fusi -o /tmp/fusi
 sudo chmod +x /tmp/fusi
-sudo cp /tmp/fusi /usr/local/bin/fusi
+sudo mv /tmp/fusi /usr/local/bin/fusi
 
 echo "© Fusi updated successfully!"
