3231737b183dcc94b6c84b5245819f2834d8f2e9
Fusiontech.rs <hesbackfrfr@gmail.com>
2026-03-31 05:54:10 +0200

    Modify install script for Fusi installation
    Updated repository name and added build and install commands.

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
-git clone https://github.com/fusiontech21/Fus-UtilityTool
-cd Fus-UtilityTool
+git clone https://github.com/fusiontech21/Fusi
+cd Fusi
 cargo build --release
 sudo cp target/release/fusi /usr/local/bin/
 echo "© Fusi installed successfully!"
